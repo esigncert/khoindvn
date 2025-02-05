13f4e21ef3eeb06cbfcdbce34daec13192c676b0
khoindvn <ios.khoindvn@gmail.com>
2025-02-06 00:13:22 +0700

    Add files via upload
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

---
+++ document/hasder.js
@@ -0,0 +1,8 @@
+/***********************************************
+> deleteHeader by khoindvn
+***********************************************/	
+
+const version = 'V1.0.2';
+
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
