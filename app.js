a845dd8bcccc58a75c35f7ba9ddf9636b8b33cc4
khoindvn <ios.khoindvn@gmail.com>
2025-02-23 15:41:07 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const apps = [
     {
         name: 'Liên Quân Hack Map ✅',
-        url: 'https://sub4unlock.io/IJCxp',
+        url: 'https://sub4unlock.io/7XF9J',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Hack Map - Mod skin Cập Nhật Ngày 23/02/2025'
     },
