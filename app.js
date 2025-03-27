4576b928ce904f7a30ce4d253543409582453ff7
khoindvn <ios.khoindvn@gmail.com>
2025-03-27 22:15:54 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 const apps = [
     {
         name: '𝙇𝙞𝙚̂𝙣 𝙌𝙪𝙖̂𝙣 𝘽𝙖̉𝙣 Hack Map ✅',
-        url: 'https://sub4unlock.io/LZVl0',
+        url: 'https://sub4unlock.io/4gT2H',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Hack Map Đánh Kín Không Phang Bụi Và Mod Skin Cập Nhật Ngày 24/03/2025'
+        descriptions: 'Hack Map Đánh Kín Không Phang Bụi Và Mod Skin Cập Nhật Ngày 28/03/2025'
     },
     {
         name: '𝙆𝙚𝙮 𝙇𝙞𝙚̂𝙣 𝙌𝙪𝙖̂𝙣 𝘽𝙖̉𝙣 Hack Map',
         url: 'https://sub4unlock.io/1giNX',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'cập nhật ngày 24/03/2025'
+        descriptions: 'cập nhật ngày 28/03/2025'
     },
     {
         name: '𝙇𝙞𝙚̂𝙣 𝙌𝙪𝙖̂𝙣 𝘽𝙖̉𝙣 Mod Skin ✅',
