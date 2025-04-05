42a5325eb4e764b3c664492e042afe02be39e235
khoindvn <ios.khoindvn@gmail.com>
2025-04-05 20:12:09 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ const apps = [
     },
     {
         name: 'TrollStore X ✅',
-        url: 'https://sub4unlock.io/IQG3M',
+        url: 'https://sub4unlock.io/8lBnK',
         img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
-        descriptions: 'Support iOS 14.0-16.6.1 Cập Nhật Ngày 27/03/2025'
+        descriptions: 'Support iOS 14.0-16.6.1 Cập Nhật Ngày 05/04/2025'
     },
     {
         name: 'Coin Master Speed✅',
