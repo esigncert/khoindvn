d0e72b4010a12ac87124981143cdd32f0a8a58dc
khoindvn <ios.khoindvn@gmail.com>
2025-06-25 11:01:20 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 const apps = [
     {
         name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Hack Map ✅ ',
-        url: 'https://sub4unlock.io/fr3XH',
+        url: 'https://sub4unlock.io/odBxt',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Hack Map Đánh Kín Update antiband Cập Nhật 24/06/2025'
+        descriptions: 'Hack Map Đánh Kín Update antiband Cập Nhật 25/06/2025'
     },
     {
         name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Mod Skin ✅',
-        url: 'https://sub4unlock.io/Xt706',
+        url: 'https://sub4unlock.io/YNMOe',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Bản Mod Skin Update antiband cập nhật 24/06/2025'
+        descriptions: 'Bản Mod Skin Update antiband cập nhật /06/2025'
     },
     {
         name: 'TrollStore X ✅',
