4864a3d88f3792fe0b229a8f1115cd30ffb4b89a
khoindvn <ios.khoindvn@gmail.com>
2025-07-19 10:27:23 +0700

    Add files via upload
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// APP DATA CONFIGURATION
 
 const apps = [
     {
@@ -24,88 +25,52 @@ const apps = [
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Gồm file 2 iPA Map Và Mod Skin Cập Nhật Ngày 18/07/2025'
     },
-]
+];
 
-const esigns = [ 
+const ksignApps = [
     {
-        name: 'ESign',
-        url: 'https://tinyurl.com/3x96trkc',
-        img: 'img/pf/profile-pic.png',
-        descriptions: 'Beijing Esensoft Co., Ltd'   
-    }
-    ,{
         name: 'KSign',
         url: 'https://tinyurl.com/324k5bp2',
         img: 'img/pf/ksign.png',
         descriptions: 'Beijing Esensoft Co., Ltd'   
-    }
-    ,{
-        name: 'ESign',
-        url: 'https://tinyurl.com/y9j5ujv8',
-        img: 'img/pf/profile-pic.png',
-        descriptions: 'NREH ESTATE INFORMATION TECHNOLOGY CO.,LTD'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://tinyurl.com/39tnn2kh',
         img: 'img/pf/ksign.png',
         descriptions: 'NREH ESTATE INFORMATION TECHNOLOGY CO.,LTD'   
-    }
-    ,{
-        name: 'ESign',
-        url: 'https://tinyurl.com/6f6aesdw',
-        img: 'img/pf/profile-pic.png',
-        descriptions: 'China Railway Eryuan Engineering Group Co., Ltd.'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://tinyurl.com/arf23bdd',
         img: 'img/pf/ksign.png',
         descriptions: 'China Railway Eryuan Engineering Group Co., Ltd'   
-    }
-    ,{
-        name: 'ESign',
-        url: 'https://tinyurl.com/3fpdcf6b',
-        img: 'img/pf/profile-pic.png',
-        descriptions: 'Dtt Technology Co.,Ltd.'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://tinyurl.com/3x87vnc9',
         img: 'img/pf/ksign.png',
         descriptions: 'Dtt Technology Co.,Ltd.'   
-    }
-    ,{
-        name: 'ESign',
-        url: 'https://tinyurl.com/4fz7uajp',
-        img: 'img/pf/profile-pic.png',
-        descriptions: 'TCL household Appliance Marketing Co., LTD'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://tinyurl.com/yc6pjsjf',
         img: 'img/pf/ksign.png',
         descriptions: 'TCL household Appliance Marketing Co., LTD'   
-    }
-    ,{
-        name: 'ESign',
-        url: 'https://api.khoindvn.eu.org/qqlFXY',
-        img: 'img/pf/profile-pic.png',
-        descriptions: 'Wuling Power Corporation'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://api.khoindvn.eu.org/yotTIY',
         img: 'img/pf/ksign.png',
         descriptions: 'Wuling Power Corporation'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://api.khoindvn.eu.org/7a8p95',
         img: 'img/pf/ksign.png',
         descriptions: 'EEO Education Ltd'   
-    }
-    ,{
+    },
+    {
         name: 'KSign',
         url: 'https://api.khoindvn.eu.org/LwV4zG',
         img: 'img/pf/ksign.png',
@@ -123,13 +88,13 @@ const esigns = [
         img: 'img/pf/ksign.png',
         descriptions: 'Vietnam Technological and Commercial Joint- stock Bank'   
     },
-    ,{
+    {
         name: 'KSign',
         url: 'https://api.khoindvn.eu.org/jvIbwS',
         img: 'img/pf/ksign.png',
         descriptions: 'VIETTEL GROUP'   
     },
-    ,{
+    {
         name: 'KSign V1',
         url: 'https://api.khoindvn.eu.org/djQtgO',
         img: 'img/pf/ksign.png',
@@ -140,7 +105,45 @@ const esigns = [
         url: 'https://api.khoindvn.eu.org/9qyWff',
         img: 'img/pf/ksign.png',
         descriptions: 'VIETTEL GROUP KSign V2'
-    
+    }
+];
+
+const esignApps = [
+    {
+        name: 'ESign',
+        url: 'https://tinyurl.com/3x96trkc',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Beijing Esensoft Co., Ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://tinyurl.com/y9j5ujv8',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'NREH ESTATE INFORMATION TECHNOLOGY CO.,LTD'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://tinyurl.com/6f6aesdw',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'China Railway Eryuan Engineering Group Co., Ltd.'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://tinyurl.com/3fpdcf6b',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Dtt Technology Co.,Ltd.'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://tinyurl.com/4fz7uajp',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'TCL household Appliance Marketing Co., LTD'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://api.khoindvn.eu.org/qqlFXY',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Wuling Power Corporation'   
     },
     {
         name: 'ESign',
@@ -192,39 +195,22 @@ const esigns = [
     }
 ];
 
+// UI COMPONENTS
 
+const cloudIcon = `<svg class="icon" style="width: 24px;height: 24px;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="739"><path d="M691.3 693.9c-24.7 0-44.8-20-44.8-44.8 0-24.7 20-44.8 44.8-44.8h40c190.6 0 176.8-274.4 0.2-268.1C671.2 37.2 208 145.1 299 445c-93-73.9-204.6 69.2-109.6 139.8 67.2 49.9 188.5-18.4 188.5 64.3 0 56.3-63.9 44.8-125.3 44.8-149.3 0-241.3-159.3-155.8-281.1 40.8-58.2 104.9-72.6 104.9-72.6C231.4 111.3 492.7-9.5 686.2 120c83.1 55.6 114.5 136.9 114.5 136.9 48.8 14.8 90.7 45.6 119.2 86.4 96 137.3 21.5 350.6-228.6 350.6zM514.2 335.8c0.2 0 0.3 0.1 0.5 0.1s0.3-0.1 0.5-0.1h-1z m177.1 421.1c0-17.5-45.8-17.3-63.3 0.1l-32.8 32.9c-14.1 14.1-38.2 4.2-38.2-15.8V380.6c0-24.5-17.9-44.4-42.3-44.7-24.5 0.3-47.2 20.1-47.2 44.7v393.5c0 19.9-24.1 29.9-38.2 15.8l-32.8-42.1c-17.5-17.5-45.3-9.3-62.8-9.3h-0.2c-17.5 44.8-17.6 55.1-0.1 72.6L449 931.5c35 35 91.6 37.3 126.6 2.3l115.7-114.6c17.5-17.5 0-45.2 0-62.7v0.4z" p-id="740"></path></svg>`;
 
+// DOM ELEMENTS
 
-const cloudIcon = `<svg class="icon" style="width: 24px;height: 24px    ;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="739"><path d="M691.3 693.9c-24.7 0-44.8-20-44.8-44.8 0-24.7 20-44.8 44.8-44.8h40c190.6 0 176.8-274.4 0.2-268.1C671.2 37.2 208 145.1 299 445c-93-73.9-204.6 69.2-109.6 139.8 67.2 49.9 188.5-18.4 188.5 64.3 0 56.3-63.9 44.8-125.3 44.8-149.3 0-241.3-159.3-155.8-281.1 40.8-58.2 104.9-72.6 104.9-72.6C231.4 111.3 492.7-9.5 686.2 120c83.1 55.6 114.5 136.9 114.5 136.9 48.8 14.8 90.7 45.6 119.2 86.4 96 137.3 21.5 350.6-228.6 350.6zM514.2 335.8c0.2 0 0.3 0.1 0.5 0.1s0.3-0.1 0.5-0.1h-1z m177.1 421.1c0-17.5-45.8-17.3-63.3 0.1l-32.8 32.9c-14.1 14.1-38.2 4.2-38.2-15.8V380.6c0-24.5-17.9-44.4-42.3-44.7-24.5 0.3-47.2 20.1-47.2 44.7v393.5c0 19.9-24.1 29.9-38.2 15.8l-32.8-42.1c-17.5-17.5-45.3-9.3-62.8-9.3h-0.2c-17.5 44.8-17.6 55.1-0.1 72.6L449 931.5c35 35 91.6 37.3 126.6 2.3l115.7-114.6c17.5-17.5 0-45.2 0-62.7v0.4z" p-id="740"></path></svg>`
 const appContainer = document.querySelector('.app-section');
+const ksignContainer = document.querySelector('.ksign-section');
 const esignContainer = document.querySelector('.esign-section');
 
-apps.forEach(app => {
-    appContainer.innerHTML += render(app.name, app.url, app.img, app.descriptions);
-});
-esigns.forEach(esign => {
-    esignContainer.innerHTML += render(esign.name, esign.url, esign.img, esign.descriptions);
-});
+// UTILITY FUNCTIONS
 
-appContainer.addEventListener('click', (event) => {
-    const target = event.target.closest('.app-container');
-    if (target) {
-        const url = target.querySelector('.download-btn').getAttribute('href');
-        window.open(url, '_blank');
-    }
-});
-
-esignContainer.addEventListener('click', (event) => {
-    const target = event.target.closest('.app-container');
-    if (target) {
-        const url = target.querySelector('.download-btn').getAttribute('href');
-        window.open(url, '_blank');
-    }
-});
 function render(name, url, img, description) {
-    return  `<div class="app-container">
+    return `<div class="app-container">
         <div class="app">
-            <img src="${img}" class="app-img box-shadow" alt="">
+            <img src="${img}" class="app-img box-shadow" alt="${name}">
             <h1 style="margin-left: 1em">${name}<br>
                 <p>${description}</p>
             </h1>
@@ -232,5 +218,43 @@ function render(name, url, img, description) {
         <i href="${url}" class="download-btn" style="color: #007aff;">
             ${cloudIcon}
         </i>
-    </div>`
+    </div>`;
 }
+
+function addClickHandler(container) {
+    if (!container) return;
+    
+    container.addEventListener('click', (event) => {
+        const target = event.target.closest('.app-container');
+        if (target) {
+            const url = target.querySelector('.download-btn').getAttribute('href');
+            window.open(url, '_blank');
+        }
+    });
+}
+
+// INITIALIZATION
+
+// Render apps in their respective containers
+if (appContainer) {
+    apps.forEach(app => {
+        appContainer.innerHTML += render(app.name, app.url, app.img, app.descriptions);
+    });
+}
+
+if (ksignContainer) {
+    ksignApps.forEach(ksign => {
+        ksignContainer.innerHTML += render(ksign.name, ksign.url, ksign.img, ksign.descriptions);
+    });
+}
+
+if (esignContainer) {
+    esignApps.forEach(esign => {
+        esignContainer.innerHTML += render(esign.name, esign.url, esign.img, esign.descriptions);
+    });
+}
+
+// Add click handlers to all containers
+addClickHandler(appContainer);
+addClickHandler(ksignContainer);
+addClickHandler(esignContainer);
