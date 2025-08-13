62b01e18681c95aa3cbe5b7a89b416e1b1d64e23
khoindvn <ios.khoindvn@gmail.com>
2025-08-13 15:12:45 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -3,38 +3,38 @@
 const apps = [
     {
         name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Hack Map ✅ ',
-        url: 'https://sub4unlock.io/6Wujc',
+        url: 'https://sub4unlock.io/UMshx',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Hack Map Đánh Kín Update antiband Cập Nhật 11/8/2025'
+        descriptions: 'Hack Map Đánh Kín Update antiband Cập Nhật 13/8/2025'
     },
     {
         name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Mod Skin ✅',
-        url: 'https://sub4unlock.io/78mpX',
+        url: 'https://sub4unlock.io/h5NHb',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Bản Mod Skin Update antiband cập nhật 11/8/2025'
+        descriptions: 'Bản Mod Skin Update antiband cập nhật 13/8/2025'
     },
     {
         name: 'TrollStore X ✅',
-        url: 'https://sub4unlock.io/rCHxp',
+        url: 'https://sub4unlock.io/BthkK',
         img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
-        descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật 11/8/2025'
+        descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật 13/8/2025'
     },
     {
         name: 'Krnl VN ✅',
-        url: 'https://sub4unlock.io/CvXZN',
+        url: 'https://sub4unlock.io/Kl5dw',
         img: 'https://loadly.io/image/view/app_icons/7a9fc85a1a0219b1bd1d1bc2af25b2de/120',
-        descriptions: 'Mod Roblox Cre Huymythic Cập Nhật 11/8/2025'
+        descriptions: 'Mod Roblox Cre Huymythic Cập Nhật 13/8/2025'
     },
     {
         name: 'DeltaVN ✅',
-        url: 'https://sub4unlock.io/ftV5e',
+        url: 'https://sub4unlock.io/qYGJa',
         img: 'https://loadly.io/image/view/app_icons/7a9fc85a1a0219b1bd1d1bc2af25b2de/120',
-        descriptions: 'Mod Roblox Cre Huymythic Cập Nhật 11/8/2025'
+        descriptions: 'Mod Roblox Cre Huymythic Cập Nhật 13/8/2025'
     },{
         name: 'File iPA Liên Quân',
-        url: 'https://sub4unlock.io/vRUnk',
+        url: 'https://sub4unlock.io/1OHvR',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Gồm file 2 iPA Map Và Mod Skin Cập Nhật Ngày 11/8/2025'
+        descriptions: 'Gồm file 2 iPA Map Và Mod Skin Cập Nhật Ngày 13/8/2025'
     },
 ];
 
