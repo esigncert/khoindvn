9d1fa3ccfbadcbf1def8ba990df3f33675af904a
khoindvn <ios.khoindvn@gmail.com>
2025-12-12 15:35:04 +0700

    Update descriptions with new dates
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const apps = [
         name: 'AovCheat Map Sáng ✅ ',
         url: 'https://sub.khoindvn.io.vn/aovcheat.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Map Sáng Đánh Kín antiband Cập Nhật Ngày 06/11/2025 Sửa'
+        descriptions: 'Map Sáng Đánh Kín antiband Cập Nhật Ngày 12/12/2025 Sửa'
     },{
         name: 'AovMod Mod Skin ✅',
         url: 'https://sub.khoindvn.io.vn/aovmod.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Mod Skin Update antiband Cập nhật Ngày 06/12/2025'
+        descriptions: 'Mod Skin Update antiband Cập nhật Ngày 12/12/2025'
     },{
         name: 'Câu Cá Vạn Cân ✅',
         url: 'https://sub.khoindvn.io.vn/cava/',
@@ -40,7 +40,7 @@ const apps = [
         name: 'File iPA Roblox',
         url: 'https://sub.khoindvn.io.vn/iparbl.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
-        descriptions: 'Gồm file 2 iPA Skibx VN Và Delta VN Cập Nhật Ngày 06/12/2025'
+        descriptions: 'Gồm file 2 iPA Skibx VN Và Delta VN Cập Nhật Ngày 12/12/2025'
     },
 ];
 
