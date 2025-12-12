a1fc28462d4100871a9f6f231d21b6727a2d30b0
khoindvn <ios.khoindvn@gmail.com>
2025-12-12 19:29:31 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const apps = [
         name: 'Delta VN ✅',
         url: 'https://sub.khoindvn.io.vn/delta.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
-        descriptions: 'Mod Roblox Cập Nhật Ngày 07/12/2025 đã Fix Lỗi Không thể cài đặt'
+        descriptions: 'Mod Roblox Cập Nhật Ngày 12/12/2025 đã Fix Lỗi Không thể cài đặt'
     },{
         name: 'Skibx VN ✅',
         url: 'https://sub.khoindvn.io.vn/krnl.html',
