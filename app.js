0dc58c79e28d4b8aab0a80e1bc8b431d14ffdab5
khoindvn <ios.khoindvn@gmail.com>
2025-12-16 14:16:36 +0700

    Fix URL format in app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const apps = [
         descriptions: 'Mod Hack Aim bot antiband Cập nhật Ngày 16/12/2025 Link Tai Trực Tiếp Không Vượt Link'
     },{
         name: 'Key Hack Crossfire: Legends ✅',
-        url: 'https://http://sub.khoindvn.io.vn/vlcf/',
+        url: 'https://sub.khoindvn.io.vn/vlcf/',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d5/67/82/d5678282-ac1b-05d7-c61f-c10955a61479/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Key Có Hạn Sử Dụng 12 Tiếng Có Vượt Link 2 Lần'
     },{
