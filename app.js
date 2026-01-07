53ddb1b066cf94c2754655e38663ccf6ade7c76f
khoindvn <ios.khoindvn@gmail.com>
2026-01-07 14:47:30 +0700

    Update app.js
    Signed-off-by: khoindvn <ios.khoindvn@gmail.com>

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const apps = [
         name: 'AovCheat Map Sáng ✅ ',
         url: 'https://sub.khoindvn.io.vn/aovcheat.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Map Sáng Đánh Kín antiband Cập Nhật Ngày 06/01/2025 Update Fix Ban Vặt'
+        descriptions: 'Map Sáng Đánh Kín antiband Cập Nhật Ngày 07/01/2025 Update Fix Ban Vặt'
     },{
         name: 'AovMod Mod Skin ✅',
         url: 'https://sub.khoindvn.io.vn/aovmod.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Mod Skin Update antiband Cập nhật Ngày 06/01/2025 Update Fix Ban Vặt'
+        descriptions: 'Mod Skin Update antiband Cập nhật Ngày 07/01/2025 Update Fix Ban Vặt'
     },{
         name: 'Crossfire: Legends ✅',
         url: 'https://i.authtool.app/ios/694cfed5a957dec377724943',
@@ -37,20 +37,15 @@ const apps = [
         img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
         descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật Ngày 25/12/2025'
     },{
-        name: 'Câu Cá Vạn Cân ✅',
-        url: 'https://sub.khoindvn.io.vn/cava/',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC9_GFjd0b8QTtE4gSCRuZmzGyR6wczYUUDDZwOG3HbQ&s=10',
-        descriptions: 'Mod Speed Tiền Vàng Cá Cập Nhật Ngày 18/12/2025'
-    },{
         name: 'Delta VN ✅',
         url: 'https://sub.khoindvn.io.vn/delta.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
-        descriptions: 'Mod Roblox v2.702.633 Cập Nhật Ngày 30/12/2025'
+        descriptions: 'Mod Roblox v2.702.633 Cập Nhật Ngày 07/01/2025'
     },{
         name: 'SkibX VN ✅',
         url: 'https://sub.khoindvn.io.vn/krnl.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
-        descriptions: 'Mod Roblox Cập Nhật Ngày 25/12/2025'
+        descriptions: 'Mod Roblox Cập Nhật Ngày 07/01/2025'
     },{
         name: 'File iPA LQMB',
         url: 'https://sub.khoindvn.io.vn/ipalqmb.html',
